041437b36d26606fb0c00296da6ab12b8acf3f2e
Maksim Nazarevskyi <ithefirstly@gmail.com>
2022-10-14 00:07:51 +0300

    Added simple model, DbContext and Home view

--- Backup_Service/Backup_Service/Controllers/HomeController.cs
+++ Backup_Service/Backup_Service/Controllers/HomeController.cs
@@ -1,28 +1,57 @@
 ﻿using Backup_Service.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Backup_Service.Data;
 
 namespace Backup_Service.Controllers
 {
     public class HomeController : Controller
     {
+        MVCDbContext _context;
+        IWebHostEnvironment _appEnvironment;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MVCDbContext context,
+            IWebHostEnvironment appEnvironment,
+            ILogger<HomeController> logger)
         {
+            _context = context;
+            _appEnvironment = appEnvironment;
             _logger = logger;
         }
-
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Files.ToList());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFileCollection uploads)
+        {
+            foreach (var uploadedFile in uploads)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Backup_Service/Backup_Service/Data/MVCDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Backup_Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backup_Service.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public DbSet<FileModel> Files { get; set; }
+        public MVCDbContext(DbContextOptions<MVCDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
